03f3351fe84c40372c85720add8c8aa3102097b9
yuan <yuanqixun@gmail.com>
2011-08-17 17:00:38 +0800

    测试类路径资源遍历加载问题。

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,8 +1,13 @@
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.StringWriter;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 
 import com.yahoo.platform.yui.compressor.JavaScriptCompressor;
 
@@ -16,18 +21,32 @@ public class Test {
 	 */
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		StringWriter sw = new StringWriter();
+		System.out.println("--------------");
+		ResourcePatternResolver rps = new PathMatchingResourcePatternResolver();
+		try {
+//			Resource[] result = rps.getResources("classpath*:/**/*.cfg.xml");
+			Resource[] result = rps.getResources("classpath*:/**/MANIFEST.MF");
+			for (int i = 0; i < result.length; i++) {
+				System.out.println(result[i]);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		System.out.println("--------------");
 		
 		
-        sw.append("/* ").append("aaaa").append(" */\n");
-		String path = "src/main/webapp/editor/scripts/Core/SVG/label.js";
-		InputStream input = new FileInputStream(new File(path));
-		JavaScriptCompressor compressor = new JavaScriptCompressor(
-				new InputStreamReader(input), null);
-		System.out.println(compressor.hashCode());
-		compressor.compress(sw, -1, false, false, false, false);
-		input.close();
-		System.out.println(sw.toString());
+//		StringWriter sw = new StringWriter();
+		
+		
+//        sw.append("/* ").append("aaaa").append(" */\n");
+//		String path = "src/main/webapp/editor/scripts/Core/SVG/label.js";
+//		InputStream input = new FileInputStream(new File(path));
+//		JavaScriptCompressor compressor = new JavaScriptCompressor(
+//				new InputStreamReader(input), null);
+//		System.out.println(compressor.hashCode());
+//		compressor.compress(sw, -1, false, false, false, false);
+//		input.close();
+//		System.out.println(sw.toString());
 		
 	}
 
